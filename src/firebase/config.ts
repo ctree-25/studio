0adec4daeb83727961d80ed07b06223fad6ffed0
Connie Tao <connie.tao20@gmail.com>
2026-01-02 02:01:43 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1657413326-a9a15",
+  "appId": "1:789680321604:web:1bfaff6f77dfaef406f811",
+  "apiKey": "AIzaSyBz9wwVinPY2t4i4SRaptuzc9yIuQdtaHc",
+  "authDomain": "studio-1657413326-a9a15.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "789680321604"
+};
