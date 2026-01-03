f6baad1cdb3bf9ddfaed9feb152457b04dc679fc
Connie Tao <connie.tao20@gmail.com>
2026-01-03 15:36:40 +0000

    When submitting profile image, I ran into Firebase Storage: No default b

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-1657413326-a9a15",
   "appId": "1:789680321604:web:1bfaff6f77dfaef406f811",
+  "storageBucket": "studio-1657413326-a9a15.appspot.com",
   "apiKey": "AIzaSyBz9wwVinPY2t4i4SRaptuzc9yIuQdtaHc",
   "authDomain": "studio-1657413326-a9a15.firebaseapp.com",
   "measurementId": "",
